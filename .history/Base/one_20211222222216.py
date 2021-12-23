fc62b9ca39476640af4c5adcd682c7400b79583c
Eidmone Tagaca <eidmonetagaca@gmail.com>
2021-12-22 22:23:17 -0800

    first commit

---
+++ .history/Base/one_20211222222216.py
@@ -0,0 +1,96 @@
+# from math import * 
+# name = "eidmone"
+# age = 13
+# isTrue = True
+# print(name)
+# # math functions
+
+# # sqrt
+# # ceil
+# # floor
+# # max
+# # min
+# # abs
+# # pow
+
+# print("hello\nworld")
+
+# print(name.upper().isupper())
+# print(len(name))
+
+# print(name[0])
+
+
+# print(name.index("e"))
+# name = name.replace("eidmone", "johnson")
+# print(name)
+
+# num=-5;
+
+# print(str(num) + "this is num")
+
+# print(abs(num))
+
+# print(pow( 2 ,3))
+
+# hello1 = 0
+
+# hello1 = min(2, 5)
+
+# print(str(hello1) + "this is hello1") 
+
+# print(ceil(3.2))
+
+# =================BASic input =================
+# name = input("Please enter your information: ")
+# age = input("age please: ")
+# print("hello " + name + " " + str(age)) #
+
+
+
+# num1 = input("Enter 1: ")
+# num2 = input("Enter 2: ")
+
+
+# res = float(num1) + float(num2)
+# print(res)
+
+# clr = input("Enter a clr: ")
+# nn = input("Enter a nn: ")
+# clb = input("Enter a clb: ")
+
+
+# print("Roses are " + clr + "\n" + nn + " are blue\n" + "I love " + clb)
+
+
+frnds = ["Jeff", "Jake", "Manny"]
+
+numbers = [1, 5, 2, 5, 7, 3]
+
+#frnds[0] = "Eidmone"
+# frnds.extend(numbers)
+# print(frnds)
+
+
+frnds.insert(0, "Eidmone")
+
+print(frnds)
+
+
+print(frnds)
+
+
+print(frnds.count("Jake"))
+print(frnds.index("Jeff"))
+
+frnds.reverse()
+
+print(frnds)
+
+
+
+frnds2 = frnds.copy()
+
+print(frnds2)
+
+
